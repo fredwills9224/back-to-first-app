e6bc0d4fede3051f02ba576cc10076149becee5d
fredwills9224 <willsf9224@gmail.com>
2021-08-09 11:47:32 -0400

    parsing url mod + queryString mod + regex

--- index.js
+++ index.js
@@ -5,11 +5,30 @@
 
 // Dependencies
     var http = require('http');
+    var url = require('url');
 // Dependencies
 
 // The server should respond to all request with a string
     var server = http.createServer(function(req, res){
-        res.end('\nHello World\n')
+        
+        // Get the URL and parse it
+            // url module calls queryString module (parameter set to [true] in [url.parse()]) 
+            // both are used to create an object with alot of keys containing metadata
+            var parsedUrl = url.parse(req.url, true);
+        // Get the URL and parse it
+        // Get the path
+            var path = parsedUrl.pathname;
+            // string replacement using regex
+                var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+            // string replacement using regex
+        // Get the path
+        // Send the response
+            res.end('\nHello World\n');
+        // Send the response
+        // Log the request
+            console.log('Request received on path: ' +trimmedPath);
+        // Log the request
+
     });
 // The server should respond to all request with a string
 
