5efe3912729b03a0d7a37b4926cf5b7a771fe992
fredwills9224 <willsf9224@gmail.com>
2021-08-09 15:22:14 -0400

    [data] from [req] and routing to [handlers]

--- index.js
+++ index.js
@@ -44,18 +44,59 @@
                 req.on('end', function(){
                     
                     buffer += decoder.end();
-                    // Send the response
-                        res.end('\nHello World\n');
-                    // Send the response
-                    // Log the request
-                        console.log(
-                            'Request received on path: ' +trimmedPath+
-                            ', with method: ' +method+
-                            ', with these query string parameters: ', 
-                            queryStringObject, headers, buffer
-                            )
-                        ;
-                    // Log the request
+                    // Choose the handler this request should go to
+                        chosenHandler = 
+                            // if [path] is not undefined set [chosenHandler] to [handler.[path] or to [handlers.notFound]
+                                typeof(router[trimmedPath]) !== 'undefined' ? 
+                                router[trimmedPath] : handlers.notFound
+                            // if [path] is not undefined set [chosenHandler] to [handler.[path] or to [handlers.notFound]
+                        ; 
+                    // Choose the handler this request should go to
+                    // Construct the data object to send to the handler
+                        var data = {
+                            'trimmedPath' : trimmedPath,
+                            'queryStringObject' : queryStringObject,
+                            'method' : method,
+                            'headers' : headers,
+                            'payload' : buffer
+                        };
+                    // Construct the data object to send to the handler
+                    // Route the request to the handler specified in the router
+                        
+                        chosenHandler(data, function(statusCode, payload){
+                            
+                            // Use the status code called back by the handler, or default to 200
+                                statusCode = 
+                                    typeof(statusCode) == 'number' ?
+                                    statusCode : 200
+                                ;
+                            // Use the status code called back by the handler, or default to 200
+                            // Use the payload called back by the handler, or default to empty object
+                                payload = 
+                                    typeof(payload) == 'object' ?
+                                    payload : {}
+                                ;
+                            // Use the payload called back by the handler, or default to empty object
+                            // Convert the payload to a string
+                                var payloadString = JSON.stringify(payload)
+                            // Convert the payload to a string
+                            // Send the response
+                                res.writeHead(statusCode);
+                                res.end(payloadString);
+                            // Send the response
+                            // Log the request
+                                console.log(
+                                        'Request received on path: ' +trimmedPath+
+                                        ', with method: ' +method+
+                                        ', with these query string parameters: ', 
+                                        queryStringObject, headers, buffer,
+                                        statusCode,payloadString
+                                    )
+                                ;
+                            // Log the request
+                        });
+
+                    // Route the request to the handler specified in the router
     
                 });
             // on the ['end'] event
@@ -69,3 +110,30 @@
         console.log("the server is listening on port 3000 now");
     });
 // Start the server, and have it listen on port 3000
+
+// Define the handlers (everything parsed from request will be sent to a handler as [data])
+    
+    var handlers = {};
+    // Sample handler
+        handlers.sample = function(data, callback){
+            // Callback a http status code, and a payload object
+                callback(406, {'name' : 'sample handler'} );
+            // Callback a http status code, and a payload object
+        };
+    // Sample handler
+
+    // Not found handler
+        handlers.notFound = function(data, callback){
+            // Callback a http status code notFound
+                callback(404);
+            // Callback a http status code notFound
+        };
+    // Not found handler
+
+// Define the handlers (everything parsed from request will be sent to a handler as [data])
+
+// Define a request router
+    var router = {
+        'sample' : handlers.sample
+    }
+// Define a request router
