d951f9017858d7d1dac6116749617d6922dcee12
fredwills9224 <willsf9224@gmail.com>
2021-08-09 12:22:57 -0400

    parsing queryStringObject from url

--- index.js
+++ index.js
@@ -22,6 +22,9 @@
                 var trimmedPath = path.replace(/^\/+|\/+$/g,'');
             // string replacement using regex
         // Get the path
+        // Get the query string as an object
+            var queryStringObject = parsedUrl.query;
+        // Get the query string as an object
         // Get the HTTP Method
             var method = req.method.toLowerCase();
         // Get the HTTP Method
@@ -29,7 +32,24 @@
             res.end('\nHello World\n');
         // Send the response
         // Log the request
-            console.log('Request received on path: ' +trimmedPath+ ' with method: ' +method);
+            /* PS C:\repos\node-js\back-to-first-app> node index.js
+                the server is listening on port 3000 now
+                C:\repos\node-js\back-to-first-app\index.js:38
+                                ' with these query string parameters: ' +queryStringObject
+                                                                        ^
+                TypeError: Cannot convert object to primitive value
+                    at Server.<anonymous> (C:\repos\node-js\back-to-first-app\index.js:38:57)
+                    at Server.emit (events.js:315:20)
+                    at parserOnIncoming (_http_server.js:874:12)
+                    at HTTPParser.parserOnHeadersComplete (_http_common.js:126:17)
+            */
+            console.log(
+                'Request received on path: ' +trimmedPath+
+                ', with method: ' +method+
+                ', with these query string parameters: ', queryStringObject
+                //                                     ^
+                )
+            ;
         // Log the request
 
     });
