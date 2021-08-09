cda57a0bd31d66a67486c14b4980918f08b41bbf
fredwills9224 <willsf9224@gmail.com>
2021-08-09 13:58:37 -0400

    binding to events omitted from streams (+=payload)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
     var http = require('http');
     var url = require('url');
+    var StringDecoder = require('string_decoder').StringDecoder;
 // Dependencies
 
 // The server should respond to all request with a string
@@ -31,16 +32,35 @@
         // Get the headers as an Object
             var headers = req.headers
         // Get the headers as an Object
-        // Send the response
-            res.end('\nHello World\n');
-        // Send the response
-        // Log the request
-            console.log(
-                headers
-                )
-            ;
-        // Log the request
-
+        // Get the payload if any
+            var decoder = new StringDecoder('utf-8');
+            var buffer = ''; // (payload) or collection of incoming streams from [decoder] 
+            // on the ['data'] event that is omitted when [decoder] has appended all streams of [data] to [buffer]
+                req.on('data', function(data){
+                    buffer += decoder.write(data);
+                });
+            // on the ['data'] event that is omitted when [decoder] has appended all streams of [data] to [buffer]
+            // on the ['end'] event
+                req.on('end', function(){
+                    
+                    buffer += decoder.end();
+                    // Send the response
+                        res.end('\nHello World\n');
+                    // Send the response
+                    // Log the request
+                        console.log(
+                            'Request received on path: ' +trimmedPath+
+                            ', with method: ' +method+
+                            ', with these query string parameters: ', 
+                            queryStringObject, headers, buffer
+                            )
+                        ;
+                    // Log the request
+    
+                });
+            // on the ['end'] event
+        // Get the payload if any
+        
     });
 // The server should respond to all request with a string
 
